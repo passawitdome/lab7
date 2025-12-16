fe297fe3deeac7dd0ccb269b6a4b85b4d08ddeec
dpassawit <nitee.dome@gmail.com>
2025-12-16 10:49:21 +0700

    lab7.cpp completed

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,19 +1,13 @@
 #include<iostream>
 using namespace std;
 
-char before(char x){
-	//Write your function definition here
-}
-
-int main(){
-	//Test Case
-	cout << before('A') << "\n";
-	cout << before('B') << "\n";
-	cout << before('P') << "\n";
-	cout << before('T') << "\n";
-	cout << before('Z') << "\n";
-	cout << before('a') << "\n";
-	cout << before('0') << "\n";
-	cout << before('c') << "\n";
-	return 0;
+char before(char c) {
+    if (c >= 'A' && c <= 'Z') {
+        if (c == 'A') {
+            return 'Z';
+        } else {
+            return c - 1;
+        }
+    }
+    return '0';
 }
